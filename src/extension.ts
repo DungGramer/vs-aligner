f962b28639f4fac8e6605ff4744b9d8b3a548ec0
DungGramer <dung.dev.gramer@gmail.com>
2024-04-14 16:16:51 +0700

    Add custom delimiter

--- src/extension.ts
+++ src/extension.ts
@@ -1,78 +1,100 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  // console.log('Congratulations, your extension "vs-aligner" is now active!');
   const disposable = vscode.commands.registerCommand("vs-aligner.align", () => {
     const editor = vscode.window.activeTextEditor;
-    const document = editor?.document;
-
-    const selection = vscode.window.activeTextEditor?.selection;
-
-    const startLine = selection?.start.line;
-    const endLine = selection?.end.line;
-
-    const commentInfoArray = [];
-
-    if (!endLine || !document || !editor || !startLine) {
+    if (!editor) {
       return;
     }
+    const document = editor.document;
+    const selection = editor.selection;
 
+    const delimiters = vscode.workspace
+      .getConfiguration("commentAligner")
+      .get<string[]>("delimiter") || ["//"];
+    const startLine = selection.start.line;
+    const endLine = selection.end.line;
+
+    const lines = [];
     for (let line = startLine; line <= endLine; line++) {
-      const curLineText = document.lineAt(line).text;
-      const commentInfo = getCommentInfo(curLineText);
-
-      commentInfoArray.push(commentInfo);
+      lines.push(document.lineAt(line).text);
     }
 
-    const maxCommentIndex = Math.max(
-      ...commentInfoArray.map((info) => info.commentIndex)
-    );
+    const alignedLines = alignLines(lines, delimiters);
 
-    const newTextArray = commentInfoArray.map((info) => {
-      if (info.commentIndex === -1) {
-        return info.textWithoutComment;
-      }
-      // return info.textWithoutComment.padEnd(maxCommentIndex, ' ') + info.comment;
-			return info.textWithoutComment + addBlankString(maxCommentIndex - info.commentIndex) + info.comment;
-    });
-
-    const newText = newTextArray.join("\n");
-    const range = new vscode.Range(
-      startLine,
-      0,
-      endLine,
-      document.lineAt(endLine).text.length
-    );
+    // Trim trailing spaces from the last line
+    alignedLines[alignedLines.length - 1] =
+      alignedLines[alignedLines.length - 1].trimEnd();
 
     editor.edit((editBuilder) => {
-      editBuilder.replace(range, newText);
+      editBuilder.replace(
+        new vscode.Range(
+          startLine,
+          0,
+          endLine,
+          document.lineAt(endLine).text.length
+        ),
+        alignedLines.join("\n")
+      );
     });
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
 
-function addBlankString(num: number) {
-  const userSetting = vscode.workspace.getConfiguration('editor');
-  const userTabSize = (userSetting.get('tabSize') || 4) as number;
+function alignLines(lines: string[], delimiters: string[]): string[] {
+  const allParts = lines.map((line) =>
+    removeBadSpace(parseLine(line, delimiters))
+  );
+  const maxPartsLengths: number[] = new Array(allParts[0].length).fill(0);
 
-	const tabSize: undefined | number | null = vscode.workspace.getConfiguration('vs-aligner').get('tabSize');
-	const alignmentChar = vscode.workspace.getConfiguration('vs-aligner').get('alignmentChar', ' ');
+  // Determine the maximum length needed for each segment to align properly
+  allParts.forEach((parts) => {
+    parts.forEach((part, index) => {
+      maxPartsLengths[index] = Math.max(
+        maxPartsLengths[index],
+        part.trimEnd().length
+      );
+    });
+  });
 
-  let blankStr = "";
-  for (let i = 0; i < num; i++) {
-    blankStr += alignmentChar.repeat((tabSize || userTabSize) / 2);
-  }
-  return blankStr;
+  return allParts.map((parts) => {
+    return parts
+      .map((part, index) => {
+        part = part.trimEnd();
+        const additionalSpace = index < parts.length - 1 ? " " : ""; // Add space between parts except the last part
+        return (
+          part +
+          " ".repeat(maxPartsLengths[index] - part.length) +
+          additionalSpace
+        );
+      })
+      .join("");
+  });
 }
 
-function getCommentInfo(lineText: string) {
-  const commentIndex = lineText.lastIndexOf("//");
-  const comment = commentIndex === -1 ? "" : lineText.slice(commentIndex);
-  const textWithoutComment =
-    commentIndex === -1 ? lineText : lineText.slice(0, commentIndex);
-  return { commentIndex, comment, textWithoutComment };
+function parseLine(lineText: string, delimiters: string[]): string[] {
+  let parts = [lineText];
+  delimiters.sort((a, b) => b.length - a.length); // Sort by length to prioritize longer delimiters
+  delimiters.forEach((delimiter) => {
+    parts = parts.flatMap((part) => {
+      const regex = new RegExp(`(${escapeRegExp(delimiter)})`);
+      return part.split(regex).filter((part) => part !== "");
+    });
+  });
+  return parts;
+}
+
+function escapeRegExp(text: string) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
+
+function removeBadSpace(arr: string[]) {
+  return arr
+    .map((part, index) => {
+      return index > 1 ? part.trim() : part; //TODO: Not trim space first line because can't check this line in block or not
+    })
+    .filter(Boolean);
 }
